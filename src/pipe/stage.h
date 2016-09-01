2d25dbabf5511e52d555f510f3887045532a011c
Andrew Comminos <andrew@comminos.com>
2016-09-01 15:01:01 -0700

    wip

--- src/pipe/stage.h
+++ src/pipe/stage.h
@@ -1,37 +1,55 @@
 #ifndef QUARKE_SRC_PIPE_STAGE_H_
 #define QUARKE_SRC_PIPE_STAGE_H_
 
+#include <GLFW/glfw3.h>
 #include <memory>
 #include <string>
 
 namespace quarke {
+namespace pipe {
 
 // A rendering stage in the quarke shader pipeline. A stage is defined as a
-// shader program taking textures as input and outputting 4-component floating
-// point data to a texture.
+// shader program taking textures as input and outputting floating RGBA data
+// to a texture.
 class Stage {
  public:
-  // Initializes the stage with the given GLSL shader source.
+  // Initializes the stage with the given GLSL shaders.
   // Returns the created stage on success, otherwise returns null.
-  static std::unique_ptr<Stage> Initialize(const std::string& shader);
+  static std::unique_ptr<Stage> Initialize(GLuint vs, GLuint fs);
 
-  void SetInputTexture(const std::string& input, ) final;
+  // Binds a texture from a shader stage as input.
+  void SetInput(const std::string& name, GLuint texture) final;
 
   // Renders the stage to a texture.
-  virtual void Render() = 0;
+  void Render();
+
+  // Invalidates the shader, requiring it to be re-rendered.
+  void Invalidate() final { invalid_ = true; }
 
   GLuint texture() { return tex_; }
+  GLuint texture_format() { return GL_RGBA; }
 
  protected:
   Stage();
   ~Stage();
+
+  // Subclass render implementation, sets up the shader inputs and calls a
+  // glDraw function. Implementations should expect the stage output to be
+  // set to its output texture, and all inputs to be bound.
+  virtual void RenderImpl() = 0;
+
+  bool invalid_;
 
   GLuint prog_;
   GLuint fs_;
   GLuint vs_;
 
   GLuint fbo_;
-  GLuint tex_;
+  GLuint color_tex_;
+  GLuint depth_tex_;
 };
 
+}  // namespace pipe
 }  // namespace quarke
+
+#endif  // QUARKE_SRC_PIPE_STAGE_H_
--- src/stage.cc
+++ src/stage.cc
@@ -3,7 +3,37 @@
 namespace quarke {
 
 /* static */
-std::unique_ptr<Stage> Stage::Initialize(const std::string& shader) {
+std::unique_ptr<Stage> Stage::Initialize(GLuint vs, GLuint fs) {
+  GLuint prog = glCreateProgram();
+  glAttachShader(prog, vs);
+  glAttachShader(prog, fs);
+  glLinkProgram(prog);
+
+  GLuint fbo;
+  glGenFramebuffers(1, &fbo);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+  GLuint color_tex;
+  glGenTextures(1, &color_tex);
+  // TODO: load data
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, color_tex, 0);
+
+  GLuint depth_tex;
+  glGenTextures(1, &
+
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+    goto cleanup;
+  }
+
+cleanup:
+  glDeleteTextures(1, &color_tex);
+  glDeleteFramebuffers(1, &fbo);
+  glDeleteProgram(1, &prog);
+  return nullptr;
+}
+
+Stage::Stage(GLuint prog) : prog_(prog), invalid_(true) {
+
 }
 
 }  // namespace quarke

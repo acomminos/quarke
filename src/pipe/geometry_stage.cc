f365195a7cf165de104025c2a8b62731c7c2ca4b
Andrew Comminos <andrew@comminos.com>
2016-09-01 17:56:35 -0700

    CMake setup.

---
+++ src/pipe/geometry_stage.cc
@@ -0,0 +1,13 @@
+#include "pipe/geometry_stage.h"
+
+namespace quarke {
+namespace pipe {
+
+void GeometryStage::DrawMesh(const Mesh& mesh) {
+  // TODO:
+  // - bind texture
+  // - draw triangle strips
+}
+
+}  // namespace pipe
+}  // namespace quarke
--- src/pipe/geometry_stage.h
+++ src/pipe/geometry_stage.h
@@ -11,9 +11,19 @@ class GeometryStage {
  public:
   // Accumulates the given mesh into the geometry stage.
   void DrawMesh(const Mesh& mesh);
+
+  // Clears the G-buffer, overwriting all attachments with zeroes.
+  void Clear();
+
+  GLuint color_tex() const { return color_tex_; }
+  GLuint normal_tex() const { return normal_tex_; }
+  GLuint depth_tex() const { return depth_tex_; }
+
  private:
-  GLuint fbo;
-  GLuint 
+  GLuint fbo_;
+  GLuint color_tex_;
+  GLuint normal_tex_;
+  GLuint depth_tex_;
 };
 
 }  // namespace pipe

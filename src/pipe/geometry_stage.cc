650edf2d00695d5bbcada574aa1cc8bb9f43be84
Andrew Comminos <andrew@comminos.com>
2016-09-03 00:29:30 -0700

    Geometry stage work.

--- src/pipe/geometry_stage.cc
+++ src/pipe/geometry_stage.cc
@@ -4,14 +4,50 @@
 namespace quarke {
 namespace pipe {
 
+static const std::string vs_header = R"(
+  in vec4 position;
+)";
+
+static const std::string fs_header = R"(
+  layout(position = 0) out vec3 color;
+  layout(position = 1) out vec3 normal;
+
+)";
+
 void GeometryStage::DrawMesh(const geo::Mesh& mesh) {
   // TODO:
   // - bind texture
   // - draw triangle strips
+
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo_);
+  // note:
+  // - fragment varying outputs correspond to a color index.
+  // - glDrawBuffers specifies which buffers are to be mapped to each index
+  // - they're adjacent and monotonic, curious.
+  // - want to use glBindFragDataLocation to map to one of these buffers.
+  glDrawBuffers(2, (const GLenum[]) { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 });
 }
 
 void GeometryStage::Clear() {
+  // TODO: scoped framebuffer state
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo_);
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
 
+void GeometryStage::SetOutputSize(int width, int height) {
+  out_width_ = width;
+  out_height_ = height;
+
+  glBindTexture(GL_TEXTURE_RECTANGLE, color_tex_);
+  glTexImage2D(GL_TEXTURE_RECTANGLE, 0, color_format(), width, height, 0,
+               GL_RGBA, GL_FLOAT, nullptr);
+  glBindTexture(GL_TEXTURE_RECTANGLE, normal_tex_);
+  glTexImage2D(GL_TEXTURE_RECTANGLE, 0, normal_format(), width, height, 0,
+               GL_RGBA, GL_FLOAT, nullptr);
+  glBindTexture(GL_TEXTURE_RECTANGLE, depth_tex_);
+  glTexImage2D(GL_TEXTURE_RECTANGLE, 0, depth_format(), width, height, 0,
+               GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
 }
 
 }  // namespace pipe
--- src/pipe/geometry_stage.h
+++ src/pipe/geometry_stage.h
@@ -12,6 +12,7 @@ struct Mesh;
 namespace pipe {
 
 // Produces a G-buffer containing color, texture, depth, and normal data.
+// TODO: integrate shader template for inputs and varying outputs
 class GeometryStage {
  public:
   // Accumulates the provided mesh into the G-buffer.
@@ -20,15 +21,26 @@ class GeometryStage {
   // Clears the G-buffer, overwriting all attachments with zeroes.
   void Clear();
 
+  // Resizes the stage and clears the framebuffer.
+  void SetOutputSize(int width, int height);
+
   GLuint color_tex() const { return color_tex_; }
+  GLenum color_format() const { return GL_RGBA; }
+
   GLuint normal_tex() const { return normal_tex_; }
+  GLenum normal_format() const { return GL_RGBA; }
+
   GLuint depth_tex() const { return depth_tex_; }
+  GLuint depth_format() const { return GL_DEPTH_COMPONENT; }
 
  private:
   GLuint fbo_;
   GLuint color_tex_;
   GLuint normal_tex_;
   GLuint depth_tex_;
+
+  int out_width_;
+  int out_height_;
 };
 
 }  // namespace pipe
